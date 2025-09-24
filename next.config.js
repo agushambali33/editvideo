b86c227ef2b6880d5f1f473dbdd8289c7584cf04
Agushambali33 <akunpremiumaing@gmail.com>
2025-09-24 12:18:31 +0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    try {
-      config.experiments = config.experiments || {};
-      config.experiments.asyncWebAssembly = true;
-    } catch (e) {}
-    return config;
+
+  // aktifkan compression untuk response API
+  compress: true,
+
+  // matikan telemetry Next.js
+  telemetry: false,
+
+  // biar build aman kalau ada modul ESM
+  experimental: {
+    esmExternals: "loose",
+  },
+
+  // biar watermark.png / asset di public bisa langsung dipakai
+  images: {
+    unoptimized: true,
   },
 };
 
