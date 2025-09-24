12c20a076a331c63996fe30c202365cd04b58d43
Agushambali33 <akunpremiumaing@gmail.com>
2025-09-24 12:30:31 +0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,18 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  // aktifkan compression untuk response API
   compress: true,
 
-  // matikan telemetry Next.js
-  telemetry: false,
-
-  // biar build aman kalau ada modul ESM
   experimental: {
     esmExternals: "loose",
   },
 
-  // biar watermark.png / asset di public bisa langsung dipakai
   images: {
     unoptimized: true,
   },
